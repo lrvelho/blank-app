8b9dbdc99b39ce2c93fb736a2e854f4fe82b2201
Luís Ricardo Velho <lrvelho@gmail.com>
2025-02-22 18:15:44 -0300

    ...

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+import streamlit as st
+
+st.title("🎈 Teste 2")
+st.title("🎈 My new app3")
+st.title("🎈 My new app4")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
