589a007cbaf1362489bfea98384cccf46c7d4d1a
Luís Ricardo Velho <lrvelho@gmail.com>
2025-02-22 18:26:03 -0300

    ....

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,50 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.title("🎈 Teste 2")
-st.title("🎈 My new app3")
-st.title("🎈 My new app4")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(layout="wide")
+
+#https://github.com/lrvelho/Phyton/blob/main/dashboards.py
+
+# Com uma visão mensal
+#faturamento por unidade… 
+# tipo de produto mais vendido, contribuição por filial,
+#Desempenho das forma de pagamento…
+#Como estão as avaliações das filiais?
+
+df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
+df["Date"] = pd.to_datetime(df["Date"])
+df=df.sort_values("Date")
+
+df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+month = st.sidebar.selectbox("Mês", df["Month"].unique())
+
+df_filtered = df[df["Month"] == month]
+
+col1, col2 = st.columns(2)
+col3, col4, col5 = st.columns(3)
+
+fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Faturamento por dia")
+col1.plotly_chart(fig_date, use_container_width=True)
+
+fig_prod = px.bar(df_filtered, x="Date", y="Product line", 
+                  color="City", title="Faturamento por tipo de produto",
+                  orientation="h")
+col2.plotly_chart(fig_prod, use_container_width=True)
+
+
+city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index()
+fig_city = px.bar(city_total, x="City", y="Total",
+                   title="Faturamento por filial")
+col3.plotly_chart(fig_city, use_container_width=True)
+
+
+fig_kind = px.pie(df_filtered, values="Total", names="Payment",
+                   title="Faturamento por tipo de pagamento")
+col4.plotly_chart(fig_kind, use_container_width=True)
+
+
+city_total = df_filtered.groupby("City")[["Rating"]].mean().reset_index()
+fig_rating = px.bar(df_filtered, y="Rating", x="City",
+                   title="Avaliação")
+col5.plotly_chart(fig_rating, use_container_width=True)
