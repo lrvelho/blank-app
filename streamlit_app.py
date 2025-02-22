8a22795d4651e33a065b066765ee9f93cc5b95c3
Luís Ricardo Velho <lrvelho@gmail.com>
2025-02-22 18:08:41 -0300

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("🎈 My new app2")
+st.title("🎈 My new app3")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
